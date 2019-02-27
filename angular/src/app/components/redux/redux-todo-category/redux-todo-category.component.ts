dd4fbd6605ef8fd8181307b362aa0d501e3988d5
Stéphane Francel <contact@avine.io>
2019-02-27 16:13:08 +0100

    Restore boilerplate in <redux-todo-category>

--- angular/src/app/components/redux/redux-todo-category/redux-todo-category.component.ts
+++ angular/src/app/components/redux/redux-todo-category/redux-todo-category.component.ts
@@ -1,11 +1,23 @@
+import { of } from 'rxjs';
+
 import { Component } from '@angular/core';
+import { TodoCategory } from 'App/domains';
 
 @Component({
   selector: 'app-redux-todo-category',
   template: `
-    <app-ui-todo-category></app-ui-todo-category>
+    <app-ui-todo-category
+      [category]="category$ | async"
+      (categoryChange)="emitCategory($event)">
+    </app-ui-todo-category>
   `
 })
 export class ReduxTodoCategoryComponent {
+  category$ = of('all');
+
   constructor() { }
+
+  emitCategory(category: TodoCategory) {
+
+  }
 }
