e10c50b3d38feb9804eb31674bfebeaf5a683d58
Stéphane Francel <contact@avine.io>
2019-02-27 15:06:01 +0100

    Removing text$ in <redux-todo-add>

--- angular/src/app/components/redux/redux-todo-add/redux-todo-add.component.ts
+++ angular/src/app/components/redux/redux-todo-add/redux-todo-add.component.ts
@@ -1,25 +1,15 @@
-import { of } from 'rxjs';
-
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-redux-todo-add',
   template: `
     <app-ui-todo-add
-      [text]="text$ | async"
-      (textChange)="textChange($event)"
       (add)="add($event)">
     </app-ui-todo-add>
   `
 })
 export class ReduxTodoAddComponent {
-  text$ = of('');
-
   constructor() { }
-
-  textChange(text: string) {
-
-  }
 
   add(text: string) {
 
