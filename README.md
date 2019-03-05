49b7bdc96639199e69fadcc3cbcb655238caf565
Stéphane Francel <contact@avine.io>
2019-03-05 12:18:44 +0100

    Clean the live coding

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # redux-react-angular-vue
 
-Comparing the implementations of a "Todo List" with and without Redux, in React, Angular and Vue.
+"Todo List" live coding with and without Redux, in Angular.
 
 ## Usage
 
 ```bash
 git clone https://github.com/xebia-france/redux-react-angular-vue.git
+
+git checkout --track origin/live-coding
 
 # First, install all dependencies
 npm run install-all
@@ -17,7 +19,14 @@ npm run server
 
 # Tab 2. Start angular app
 npm run angular
-
-# Tab 3. Start react app
-npm run react
 ```
+
+## Your challenge
+
+Implement the following features using Redux:
+
+- Fetch items
+- Add item
+- Mark item as done
+- Remove item
+- Filter items by category (all, active, completed)
--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -6,11 +6,23 @@ import { Component } from '@angular/core';
     <div class="container">
       <h1 class="title">{{ title }}</h1>
 
-      <app-vanilla-todo></app-vanilla-todo>
+      <div class="navbar">
+        <nav class="nav">
+          <a class="nav-link" [class.nav-link--active]="vanilla" (click)="vanilla = true">Vanilla</a>
+          <a class="nav-link" [class.nav-link--active]="!vanilla" (click)="vanilla = false">Redux</a>
+        </nav>
+      </div>
+
+      <app-vanilla-todo *ngIf="vanilla"></app-vanilla-todo>
+      <app-redux-todo *ngIf="!vanilla"></app-redux-todo>
     </div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tout doux!';
+
+  // Just to simplify the code, there's Routing in this live coding.
+  // We are just using this boolean member to switch between "pages"...
+  vanilla = true;
 }
